a1bc4308bcb68c928d79f89b46ba134e64c04ddd
Ignacio_PerezC <ignacio.perez@datalytics.com>
2023-11-28 16:09:33 -0300

    turururuuuuu max verstrapenn

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,42 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+require 'vendor/autoload.php'; // Asegúrate de que la ruta sea correcta según tu estructura de archivos
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Datos del formulario
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $mail = new PHPMailer(true);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    try {
+        // Configuración del servidor SMTP de Gmail
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'code.assurance.QA@gmail.com'; // Tu dirección de correo de Gmail
+        $mail->Password = 'bfvh ajkd xedf tvvg'; // La contraseña de tu cuenta de Gmail
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+        // Destinatario del correo electrónico
+        $mail->setFrom($email, $name);
+        $mail->addAddress('code.assurance.QA@gmail.com'); // Reemplaza con la dirección a la que deseas enviar el correo
+        $mail->Subject = $subject;
+        $mail->Body = "<strong>Mensaje de:</strong> $name ($email)<br><br>$message";
+        $mail->isHTML(true);
 
-  echo $contact->send();
+        // Enviar correo
+        $mail->send();
+        echo '¡El mensaje se ha enviado con éxito!';
+    } catch (Exception $e) {
+        echo "Hubo un problema al enviar el mensaje: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Acceso denegado.";
+}
 ?>
